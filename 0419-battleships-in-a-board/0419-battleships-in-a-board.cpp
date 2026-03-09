75e164680cfbfe52630e8a6cdd2635021bc3a323
Nishtha <148875630+Nishtha56@users.noreply.github.com>
2026-03-09 16:47:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void dfs(int &curr, int i, int j, vector<vector<char>>& board, vector<vector<int>> &vis){
+    void dfs(int &curr, int i, int j, vector<vector<char>>& board){
         int n=board.size();
         int m=board[0].size();
 
-        if(i<0 || j<0 || i>=n || j>=m || board[i][j]=='.' ||vis[i][j]==1){
+        if(i<0 || j<0 || i>=n || j>=m || board[i][j]=='.'){
             return;
         }
         board[i][j]='.';
 
-        dfs(curr, i-1, j, board, vis);
-        dfs(curr, i+1, j, board, vis);
-        dfs(curr, i, j-1, board, vis);
-        dfs(curr, i, j+1, board, vis);
+        dfs(curr, i-1, j, board);
+        dfs(curr, i+1, j, board);
+        dfs(curr, i, j-1, board);
+        dfs(curr, i, j+1, board);
 
     }
     int countBattleships(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
+      
         int curr=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]=='X'){
-                    dfs(curr, i, j, board, vis);
+                    dfs(curr, i, j, board);
                     curr++;
                     
                 } 
