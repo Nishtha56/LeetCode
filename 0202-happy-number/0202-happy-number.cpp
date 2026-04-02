d7f5ee8bd294b1dd675baaf1f536755cd0033e61
Nishtha <148875630+Nishtha56@users.noreply.github.com>
2026-04-02 14:54:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.58%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n==1 || n==7){
+            return true;
+        }
+        if(n<10) return false;
+        int sum=0;
+        while(n){
+            int x=n%10;
+            sum+=(x*x);
+            n=n/10;
+        }
+        return isHappy(sum);
+    }
+};
