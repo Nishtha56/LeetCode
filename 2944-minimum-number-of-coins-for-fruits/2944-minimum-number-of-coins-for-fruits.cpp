d1f501af7a28ffaa5189173c6681cfb3bd810b6f
Nishtha <148875630+Nishtha56@users.noreply.github.com>
2026-04-04 22:02:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    int helper(int idx, vector<int> &prices, vector<int>& dp){
-        int n=prices.size();
-        if(idx>=n){
-            return 0;  // base case
-        }
+    // int helper(int idx, vector<int> &prices, vector<int>& dp){
+    //     int n=prices.size();
+    //     if(idx>=n){
+    //         return 0;  // base case
+    //     }
 
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
+    //     if(dp[idx]!=-1){
+    //         return dp[idx];
+    //     }
 
-        int ans=INT_MAX;
-        int maxi=min(n, 2*idx+2);
-        for(int i=idx+1; i<=maxi;i++){
-            ans=min(ans, helper(i, prices, dp));
-        }
-        return dp[idx]=ans+prices[idx];  // 1st element always taken
-    }
+    //     int ans=INT_MAX;
+    //     int maxi=min(n, 2*idx+2);
+    //     for(int i=idx+1; i<=maxi;i++){
+    //         ans=min(ans, helper(i, prices, dp));
+    //     }
+    //     return dp[idx]=ans+prices[idx];  // 1st element always taken
+    // }
     int minimumCoins(vector<int>& prices) {
         int n=prices.size();
-        vector<int> dp(n+1, -1);
+        vector<int> dp(n+1, 0);
 
-        int a=helper(0, prices, dp);
-        return a;
+        for(int i=n-1;i>=0;i--){
+            int ans=INT_MAX;
+            int maxi=min(n, 2*i+2);
+            for(int j=i+1; j<=maxi;j++){
+                ans=min(ans,dp[j]);
+            }
+            dp[i]=ans+prices[i];
+
+        }
+        return dp[0];
 
     }
 };
