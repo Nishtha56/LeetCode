694eda9218882ed267c608f519e120105bdd8c3c
Nishtha <148875630+Nishtha56@users.noreply.github.com>
2026-06-26 14:50:53 +0530

    Time: 8 ms (11.72%), Space: 23.4 MB (93.85%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.cpp
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -1,50 +1,53 @@
 class MyCircularQueue {
-    int front;
-    int rear;
-    int size;
-    int* queue;
-
 public:
+    int f, r, sz, cap;
+    vector<int> v;
     MyCircularQueue(int k) {
-        front = rear = 0;
-        size = k + 1;
-        queue = new int[size];
+        v.resize(k);
+        f=0;
+        r=0;
+        sz=0;
+        cap=k;
     }
     
     bool enQueue(int value) {
-        if (((rear + 1) % size) == front) return false;
-        
-        rear = (rear + 1) % size;
-        queue[rear] = value;
+        if(sz==cap){
+            return false;
+        }
+        v[r]=value;
+        sz++;
+        r++;
+        r=r%cap;
         return true;
     }
     
     bool deQueue() {
-        if (rear == front) return false;
+        if(sz==0) return false;
+        f++;
+        sz--;
+        f=f%cap;
+        return true;
         
-        front = (front + 1) % size;
-        return true;       
     }
     
     int Front() {
-        if (rear == front) return -1;
-        
-        return queue[(front + 1) % size];
+        if(sz==0) return -1;
+
+        return v[f];
     }
     
     int Rear() {
-        if (rear == front) return -1;
-        
-        return queue[rear];
-
+        if(sz==0) return -1;
+        if(r==0) return v[cap-1];
+        return v[r-1];
     }
     
     bool isEmpty() {
-        return rear == front;
+        return sz==0;
     }
     
     bool isFull() {
-        return ((rear + 1) % size) == front;
+        return sz==cap;
     }
 };
 
